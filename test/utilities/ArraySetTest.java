0c8126b4b2bd2e6b2a4db7114d8d1f4d9db34319
reganrichardson <92763866+reganrichardson@users.noreply.github.com>
2022-08-24 16:23:39 -0400

    ArraySetTest

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -1,36 +1,55 @@
 package utilities;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class ArraySetTest
+
 {
 	@Test
 	void testArraySetCollectionOfE()
 	{
+		 assertTrue(true);
 	}
 
 	@Test
 	void testAddE()
 	{
+		ArraySet.add();
+		//not in : true
+		assertTrue(true);
+		//in : false
+		
 	}
 
 	@Test
 	void testAddAllCollectionOfQextendsE()
 	{
+		
 	}
 
 	@Test
 	void testRetainAll()
 	{
+		//Get initial size
+		//Check if size changes after running
+		
 	}
 
 	@Test
 	void testRemoveAll()
 	{
+		//get initial size
+		// Check size = 0
+		
 	}
 
 	@Test
 	void testAddAllIntCollectionOfQextendsE()
 	{
+		
 	}
 }
